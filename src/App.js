16d02622ad5aa8c042cecc48d042e40a8b49282d
Adilson Mendes <adilson.mendes@capco.com>
2021-03-15 22:45:29 +0000

    adding project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://octodex.github.com/images/Professortocat_v2.png" className="App-logo" alt="logo" />
+        <p>
+         <code>Capco</code>
+        </p>
+        Engineering Framework
+      </header>
+    </div>
+  );
+}
+
+export default App;
