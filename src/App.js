8550ac8d17bd0569dca2c555f1228e9a34b486a3
Adilson Mendes <adilson.mendes@capco.com>
2021-03-16 16:19:44 +0000

    uploading file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 
 import './App.css';
+import React, {useState} from 'react';
+import * as XLSX from 'xlsx';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const handleUpload = (e) => {
+    e.preventDefault();
+
+    var files = e.target.files, f = files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        var data = e.target.result;
+        let readedData = XLSX.read(data, {type: 'binary'});
+        const wsname = readedData.SheetNames[0];
+        const ws = readedData.Sheets[wsname];
+  
+        /* Convert array to json*/
+        const dataParse = XLSX.utils.sheet_to_json(ws, {header:1});
+        console.log(dataParse);
+        setData(dataParse)
+    };
+    reader.readAsBinaryString(f)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +34,10 @@ function App() {
          <code>Capco</code>
         </p>
         Engineering Framework!!
+        <input type="file" name="file" onChange={handleUpload}/>
+        {data}
       </header>
+
     </div>
   );
 }
